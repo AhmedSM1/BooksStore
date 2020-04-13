3adafe483035f55960ffe927815b7d5f3bc69fe5
ahmed <ahmedalmansoer@gmail.com>
2020-04-13 23:45:39 +0300

    Remove DevTools

--- books_ws_query/src/main/java/com/ahmed/books_ws_query/BookQueryMain.java
+++ books_ws_query/src/main/java/com/ahmed/books_ws_query/BookQueryMain.java
@@ -5,12 +5,15 @@ import com.ahmed.books_ws_query.config.BackendConfig;
 import io.eventuate.EventHandlerMethod;
 import io.eventuate.javaclient.spring.EnableEventHandlers;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @EnableEurekaClient
-@Import(BackendConfig.class)
+@EnableAutoConfiguration
+@Configuration
 @ComponentScan
 public class BookQueryMain {
     public static void main(String[] args) {
